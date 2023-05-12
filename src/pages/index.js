b1bacafe069f0e611c687b0e2d47a77adb96c975
AndrewBiya <s2200122@student.unklab.ac.id>
2023-05-12 17:16:31 +0800

    Insert SignIn & Akun

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Home from './Home'
 import Pesanan from './Pesanan'
 import Splash from './Splash'
+import SignIn from './SignIn'
+import Akun from './Akun'
 
 
 
-export{Splash,Pesanan,Home}
+export{Splash,Pesanan,Home,SignIn,Akun}
