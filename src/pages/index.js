e0eb15e718426209559729b6a02d51017e4c7725
dhioclaudee <s2200505@student.unklab.ac.id>
2023-05-12 17:21:28 +0800

    Page SignUP

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Pesanan from './Pesanan'
 import Splash from './Splash'
 import SignIn from './SignIn'
 import Akun from './Akun'
+import SignUp from './SignUp'
 
 
 
-export{Splash,Pesanan,Home,SignIn,Akun}
+
+export{Splash,Pesanan,Home,SignIn,Akun,SignUp}
